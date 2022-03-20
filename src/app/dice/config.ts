9d6d39288e981fb79c7d4d2c3ef04bd7b1f17bdd
Nikita Korshunov <kewka96@gmail.com>
2022-03-20 16:57:59 +0500

    chore: update DICE_ADDRESS

--- src/app/dice/config.ts
+++ src/app/dice/config.ts
@@ -1,2 +1,2 @@
 export { default as DICE_ABI } from '../abis/Dice.json';
-export const DICE_ADDRESS = '0x264AAdE0a9A4eCD999912b695f6602eae8d8B0A5';
+export const DICE_ADDRESS = '0x1e33682fB924CBb2f1a73fF95d304bc0A7Aee7c0';
